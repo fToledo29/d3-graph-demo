41901d6b8778b95405c1209d214a7b2f11f4257c
Fernando Toledo <fernando_29837@live.com>
2021-06-28 17:44:30 -0500

    Removing mistakenly added folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import D3Component from './d3Component/d3Graph';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		data: [12, 5, 6, 6, 9, 10],
+		width: 700,
+		height: 500,
+		id: 'root'
+	}
+
+	render() {
+
+		const airports = ['LAX', 'ATL', 'DFW', 'ORD', 'MEX', 'AMS', 'JFK', 'LOS', 'VKO', 'IST', 'LHR','AMS', 'CUN', 'BOM', 'TYO'];
+
+		const routes = [
+			['LAX', 'ORD'],
+			['LAX', 'MEX'],
+			['MEX', 'DFW'],
+			['MEX', 'CUN'],
+			['CUN', 'DFW'],
+			['DFW', 'ATL'],
+			['DFW', 'LOS'],
+			['LOS', 'AMS'],
+			['LOS', 'VKO'],
+			['VKO', 'AMS'],
+			['VKO', 'IST'],
+			['IST', 'LHR'],
+			['AMS', 'JFK'],
+			['AMS', 'IST'],
+			['LHR', 'JFK'],
+			['JFK', 'ATL'],
+			['JFK', 'ORD'],
+			['ORD', 'ATL'],
+			['ATL', 'LAX'],
+			['ATL', 'AMS'],
+		];
+
+		return (
+			<div className="App">
+				<D3Component id="graphTest" airports={airports} routes={routes} />
+			</div>
+		);
+	};
 }
 
 export default App;
